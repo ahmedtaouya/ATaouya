9f11683f1591a91706e38cbfba9df8c2cd031e6f
ahmedtaouya <ahmad.taouya@gmal.com>
2025-08-12 19:17:19 +0100

    ..

--- app/components/me.js
+++ app/components/me.js
@@ -11,38 +11,21 @@ function Me() {
   const { t } = useTranslation();
 
   return (
-    <Container maxWidth="lg" sx={{ padding: { xs: 2, md: 3 } }}>
+    <Container maxWidth="lg" sx={{ padding: { xs: 0, md:0 } }}>
       <Box
         display="flex"
-        flexDirection={{ xs: "column", md: "row" }}
+         flexDirection={{ xs: 'column', md: 'row' }}
         alignItems="center"
         justifyContent="space-between"
-        gap={4}
+        gap={2}
       >
-        {/* Left section: scroll animation */}
         <Box sx={{ 
-          width: '100%', 
-          maxWidth: { xs: '300px', md: '750px' },
-          marginBottom: { xs: 2, md: 0 }
-        }}>
-          <Compare
-            firstImage="/images/cv.jpg"
-            secondImage="/images/cv2.jpg"
-            firstImageClassName="object-cover object-left-top"
-            secondImageClassname="object-cover object-left-top"
-            className="h-[300px] w-[300px] md:h-[500px] md:w-[750px]"
-            slideMode="hover"
-          />
-        </Box>
-
-        {/* Right section: developer information */}
-        <Box sx={{ 
-          width: '60%', 
-          maxWidth: { xs: '100%', md: '50%' },
-          paddingLeft: { xs: 8, md: 4 }
+          width: '40%', 
+          maxWidth: { xs: '100%', md: '100%' },
+          paddingLeft: { xs: 1, md: 8 }
         }}>
           <CardContainer className="inter-var">
-            <CardBody className="bg-[rgba(0,0,0,0.4)] relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] border w-[450px] rounded-xl p-6">
+            <CardBody className="bg-[rgba(0,0,0,0.4)] relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] border w-[500px] rounded-xl p-4">
               <Typography
                 variant="h4"
                 component="h1"
@@ -94,6 +77,24 @@ function Me() {
             </CardBody>
           </CardContainer>
         </Box>
+        {/* Left section: scroll animation */}
+        <Box sx={{ 
+          width: '100%', 
+          maxWidth: { xs: '300px', md: '560px' },
+          marginBottom: { xs: 4, md: 0 }
+        }}>
+          <Compare
+            firstImage="/images/cv.jpg"
+            secondImage="/images/cv2.jpg"
+            firstImageClassName="object-cover object-left-top"
+            secondImageClassname="object-cover object-left-top"
+            className="h-[300px] w-[300px] md:h-[500px] md:w-[750px]"
+            slideMode="hover"
+          />
+        </Box>
+
+        
+        
       </Box>
     </Container>
   );
